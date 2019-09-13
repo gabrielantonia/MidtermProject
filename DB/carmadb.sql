c725b8c4a193d607e6cb66d62e3a6c177b9adfaa
Connor Campora <connorcampora@Brucebs-MacBook-Pro.local>
2019-09-13 10:52:22 -0600

    fixed updating database to correct inserts

--- DB/carmadb.sql
+++ DB/carmadb.sql
@@ -52,19 +52,6 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `picture`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `picture` ;
-
-CREATE TABLE IF NOT EXISTS `picture` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `url` VARCHAR(2000) NOT NULL,
-  `parking_fail_id` INT NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `location`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `location` ;
@@ -77,6 +64,60 @@ CREATE TABLE IF NOT EXISTS `location` (
   `state` CHAR(2) NOT NULL,
   `zip` CHAR(5) NOT NULL,
   PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `parking_fail`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `parking_fail` ;
+
+CREATE TABLE IF NOT EXISTS `parking_fail` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(100) NOT NULL,
+  `car_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  `location_id` INT NOT NULL,
+  `fail_time` DATETIME NOT NULL,
+  `description` TEXT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_parking_fail_car1_idx` (`car_id` ASC),
+  INDEX `fk_parking_fail_user1_idx` (`user_id` ASC),
+  INDEX `fk_parking_fail_location1_idx` (`location_id` ASC),
+  CONSTRAINT `fk_parking_fail_car1`
+    FOREIGN KEY (`car_id`)
+    REFERENCES `car` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_parking_fail_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_parking_fail_location1`
+    FOREIGN KEY (`location_id`)
+    REFERENCES `location` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `picture`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `picture` ;
+
+CREATE TABLE IF NOT EXISTS `picture` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `url` VARCHAR(2000) NOT NULL,
+  `parking_fail_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_picture_parking_fail1_idx` (`parking_fail_id` ASC),
+  CONSTRAINT `fk_picture_parking_fail1`
+    FOREIGN KEY (`parking_fail_id`)
+    REFERENCES `parking_fail` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
@@ -94,9 +135,15 @@ CREATE TABLE IF NOT EXISTS `carma` (
   `date_voted` DATETIME NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_vote_user_idx` (`user_id` ASC),
+  INDEX `fk_vote_parking_fail_idx` (`parking_fail_id` ASC),
   CONSTRAINT `fk_vote_user`
     FOREIGN KEY (`user_id`)
     REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_vote_parking_fail`
+    FOREIGN KEY (`parking_fail_id`)
+    REFERENCES `parking_fail` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -117,9 +164,7 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `carmadb`;
-INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`, `email`, `date_user_account_created`) VALUES (1, 'jimjeffries', 'carmauser', true, 'user', 'jimjeffries@beerlover.com', '20190911');
-INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`, `email`, `date_user_account_created`) VALUES (DEFAULT, 'louisck', 'carmauser', true, 'user', 'louisck@madeupshit.com', '20190912');
-INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`, `email`, `date_user_account_created`) VALUES (DEFAULT, 'davechappelle', 'carmauser', true, 'user', 'davechappelle@holyhell.com', '20190913');
+INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`, `email`, `date_user_account_created`) VALUES (1, 'jim', 'carmauser', true, 'user', NULL, NULL);
 
 COMMIT;
 
@@ -129,45 +174,7 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `carmadb`;
-INSERT INTO `car` (`id`, `license_plate`, `make`, `model`, `color`, `description`, `alias`) VALUES (1, 'JDO D9R', 'toyota', 'camre', 'black', 'shitty ass toyota', 'big dick');
-INSERT INTO `car` (`id`, `license_plate`, `make`, `model`, `color`, `description`, `alias`) VALUES (2, 'ERC 8ER', 'honda', 'civic', 'red', 'a beat up old honda civic piece of shit', 'small dick');
-INSERT INTO `car` (`id`, `license_plate`, `make`, `model`, `color`, `description`, `alias`) VALUES (3, 'KIO NDH', 'Ford', 'explorer', 'white', 'a beautiful for explorer covered in children\'s blood', 'kid killer');
-
-COMMIT;
-
-
--- -----------------------------------------------------
--- Data for table `picture`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `carmadb`;
-INSERT INTO `picture` (`id`, `url`, `parking_fail_id`) VALUES (DEFAULT, 'https://images.yourdailydish.com/wp-content/uploads/2015/01/o-PARKING-FAIL-facebook.jpg', 1);
-INSERT INTO `picture` (`id`, `url`, `parking_fail_id`) VALUES (DEFAULT, 'http://www.philapark.org/wp-content/uploads/Parking-Fail-20.png', 2);
-INSERT INTO `picture` (`id`, `url`, `parking_fail_id`) VALUES (DEFAULT, 'https://komonews.com/resources/media/49b41b88-f3e5-4756-98de-540165cd4943-large16x9_CrashatHearthstoneSeniorLivingPhotofromBeavertonPolice4.jpg', 3);
-
-COMMIT;
-
-
--- -----------------------------------------------------
--- Data for table `location`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `carmadb`;
-INSERT INTO `location` (`id`, `name`, `street`, `city`, `state`, `zip`) VALUES (DEFAULT, 'Parklikeshitarea', 'Assholdriver Way', 'Somecity', 'CO', '80011');
-INSERT INTO `location` (`id`, `name`, `street`, `city`, `state`, `zip`) VALUES (DEFAULT, 'Rundogsoversquare', 'Harleydriver St', 'Landofpoorwhitepeople', 'OH', '72387');
-INSERT INTO `location` (`id`, `name`, `street`, `city`, `state`, `zip`) VALUES (DEFAULT, 'Drivelikegrandmaplace', 'Kidkiller Blvd', 'Assholia', 'NY', '25934');
-
-COMMIT;
-
-
--- -----------------------------------------------------
--- Data for table `carma`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `carmadb`;
-INSERT INTO `carma` (`id`, `user_id`, `parking_fail_id`, `vote`, `comment`, `date_voted`) VALUES (DEFAULT, 1, 1, 1, 'hah this idiot parked sideways in a mcdonald\'s drivethrough', '20190911');
-INSERT INTO `carma` (`id`, `user_id`, `parking_fail_id`, `vote`, `comment`, `date_voted`) VALUES (DEFAULT, 2, 2, 1, 'this moron flipped his car onto a 90 year old woman', '20190912');
-INSERT INTO `carma` (`id`, `user_id`, `parking_fail_id`, `vote`, `comment`, `date_voted`) VALUES (DEFAULT, 3, 3, 1, 'this guy rammed his truck through a wallmart where it exploded burning everyone inside alive', '20190913');
+INSERT INTO `car` (`id`, `license_plate`, `make`, `model`, `color`, `description`, `alias`) VALUES (DEFAULT, 'big coc', 'toyota', 'camre', 'black', 'freaking car cut me off', NULL);
 
 COMMIT;
 
