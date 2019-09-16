e24038f529283d0cc1d8588c220a583493e971e7
Justin Choi <jschoi1991@gmail.com>
2019-09-16 14:24:53 -0600

    Working session history with navbar

---
+++ Carma/src/main/java/com/skilldistillery/carma/controllers/AccountController.java
@@ -0,0 +1,66 @@
+package com.skilldistillery.carma.controllers;
+
+import java.time.LocalDate;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.skilldistillery.carma.data.ParkingFailDAO;
+import com.skilldistillery.carma.data.UserDAO;
+import com.skilldistillery.carma.entities.User;
+
+@Controller
+@SessionAttributes("name")
+public class AccountController {
+
+	@Autowired
+	private UserDAO dao;
+	@Autowired
+	private ParkingFailDAO parkingdao;
+
+	@RequestMapping(path = "register.do", method=RequestMethod.GET)
+	public String registerUser(Model model) {
+		model.addAttribute("user", new User());
+		return "sub/register";
+	}
+
+	@RequestMapping(path="register.do", method=RequestMethod.POST)
+	public String createUser(@ModelAttribute("user") User user, Model model) {
+		user.setDateCreated(LocalDate.now().toString());
+		dao.addUser(user);
+		model.addAttribute("user", user);
+		return "sub/userpage";
+	}
+	
+	@RequestMapping(path="login.do", method=RequestMethod.POST)
+	public String doLogin(@ModelAttribute("user") User user, Model model, Errors errors, HttpSession session) {
+		User u = dao.validateUser(user);
+		if (errors.hasErrors()) {
+			return "index";
+		}
+		if (u != null) {
+			session.setAttribute("loggedInUser", u);
+			model.addAttribute("listOfPF", parkingdao.findParkingFailByUserId(u.getId()));
+			return "sub/userpage";
+		}
+		else {
+			return "index";
+		}
+	}
+	
+	@RequestMapping(path="logout.do")
+	public String doLogout(Model model, HttpSession session) {
+		session.removeAttribute("loggedInUser");
+		return "redirect:/";
+	}
+	
+	
+}
