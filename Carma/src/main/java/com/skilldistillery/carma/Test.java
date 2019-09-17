74ad955ac666d19d9d7805f25593dc4daa2aa9fb
Gabriel Antonia <Gabriel.Antonia@Outlook.com>
2019-09-17 16:28:13 -0600

    Added Wall of Shame jsp

--- Carma/src/main/java/com/skilldistillery/carma/Test.java
+++ Carma/src/main/java/com/skilldistillery/carma/Test.java
@@ -2,12 +2,12 @@ package com.skilldistillery.carma;
 
 import com.skilldistillery.carma.data.ParkingFailDAOImpl;
 
-public class Test {
+//public class Test {
 
-	public static void main(String[] args) {
-		ParkingFailDAOImpl pf = new ParkingFailDAOImpl();
-//		System.out.println(pf.findAllPictures());
-		System.out.println(pf.findPicturesByUserId(1));
-	}
-
-}
+//	public static void main(String[] args) {
+//		ParkingFailDAOImpl pf = new ParkingFailDAOImpl();
+////		System.out.println(pf.findAllPictures());
+//		System.out.println(pf.findPicturesByUserId(1));
+//	}
+//
+//}
