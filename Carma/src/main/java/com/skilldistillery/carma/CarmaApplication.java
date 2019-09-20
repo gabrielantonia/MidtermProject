f46f9b18654b659c2c9034919db481c25f5e71de
Connor Campora <connorcampora@Brucebs-MacBook-Pro.local>
2019-09-20 15:37:57 -0600

    added .war file

--- Carma/src/main/java/com/skilldistillery/carma/CarmaApplication.java
+++ Carma/src/main/java/com/skilldistillery/carma/CarmaApplication.java
@@ -3,11 +3,16 @@ package com.skilldistillery.carma;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EntityScan("com.skilldistillery.carma")
-public class CarmaApplication extends SpringBootServletInitializer{
+public class CarmaApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CarmaApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarmaApplication.class, args);
